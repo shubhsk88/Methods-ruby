cf97974429c35df6f90b07e42566835cfc91c504
shubhsk88 <shubham88singh@gmail.com>
2020-04-30 01:20:08 +0530

    Adding and updating the my map function

--- method.rb
+++ method.rb
@@ -55,10 +55,74 @@ module Enumerable
         stat
     end
 
+    def my_count(counter=self.size)
+        p_count=0
+        if(block_given?)
+            my_each do |num|
+                if(yield num)
+                    p_count+=1
+                end
+            
+            end
+            counter=p_count
+        end
+        counter
+    end
+
+    def my_inject(acc=0)
+        
+        my_each do |num|
+            acc=yield(acc,num)
+
+        end
+        acc
+    end
+    def multiply_els
+        
+       res= my_inject(1) do |acc,num|
+            acc*num
+            
+        end
+        
+    end  
+    
+    def my_map(prop=nil)
+        new_arr=[]
+        if(prop) 
+            my_each do |num|
+                new_arr.push(prop.call(num))
+            end
+        
+        else
+            my_each do |num|
+              new_arr.push(yield num)
+            end
+        end
+
+        new_arr
+    
+        
+    end
+       
+
+
+
+        
+            
+        
+
+
 end
 
 
-arr=[4,6,4]
+arr=[4,6,3]
 
 # p arr.my_all?{|num| num%2==0} 
-p %w[ant bear cat].any? { |word| word.length >= 4 }
+# p arr.my_count(7)
+
+# p (1..4).map { |i| i*i } 
+# p arr.my_inject(0) { |sum, n| sum + n }  
+
+p arr.multiply_els
+prop=Proc.new{|x| x*2}
+p arr.my_map(prop)
