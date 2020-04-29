0e35f1f48089828276363338592b5ac090766974
shubhsk88 <shubham88singh@gmail.com>
2020-04-30 01:35:57 +0530

    Setting up the readme file

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Methods-ruby
+# Array-Method Ruby
+
+This Project showcases and implements the Array method of ruby using the Enumerable,The method represented in this project as follow
+
+- each
+- each_with_index
+- select
+- all?
+- any?
+- none?
+- count
+- map
+- inject
+
+## Built With
+
+- Ruby
+
+## Live Demo
+
+[Live Demo](https://repl.it/@shubhamsingh/Ruby-Array-Method)
+
+## Authors
+
+👤 **Shubham Singh**
+
+- Github: [@shubhsk88](https://github.com/shubhsk88)
+- Twitter: [@shubhski](twitter.com/shubski)
+- Linkedin: [linkedin](https://www.linkedin.com/in/shubham-singh-130349140/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- method.rb
+++ method.rb
@@ -1,128 +1,99 @@
 module Enumerable
-    def my_each
-      for i in 0...length
-        yield self[i]
-      end
-     self  
+  def my_each
+    (0...length).each do |i|
+      yield self[i]
     end
-
-  
-  def my_each_with_index
-        for i in 0...length
-            yield self[i],i
-        end
     self
+  end
+
+  def my_each_with_index
+    (0...length).each do |i|
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    new_arr = []
+    my_each do |num|
+      new_arr.push(num) if yield num
+    end
+    new_arr
+  end
+
+  def my_all?
+    stat = true
+    my_each do |num|
+      stat = false unless yield num
+    end
+    stat
+  end
+
+  def my_any?
+    stat = false
+    my_each do |num|
+      stat = true if yield num
+    end
+    stat
+  end
+
+  def my_none?
+    stat = true
+    my_each do |num|
+      stat = false if yield num
+    end
+    stat
+  end
+
+  def my_count(counter = size)
+    p_count = 0
+    if block_given?
+      my_each do |num|
+        p_count += 1 if yield num
+      end
+      counter = p_count
+    end
+    counter
+  end
+
+  def my_inject(acc = 0)
+    my_each do |num|
+      acc = yield(acc, num)
+    end
+    acc
+  end
+
+  def multiply_els
+    my_inject(1) do |acc, num|
+      acc * num
+    end
+  end
+
+  def my_map(prop = nil)
+    new_arr = []
+    if prop
+      my_each do |num|
+        new_arr.push(prop.call(num))
+      end
+
+    else
+      my_each do |num|
+        new_arr.push(yield num)
+      end
     end
 
-    def my_select
-        new_arr=[]
-        my_each do |num|
-            if(yield num)
-                new_arr.push(num)
-            end
-        end
-        new_arr
-    end
-    
-    def my_all?
-        stat=true
-        my_each do |num|
-            if(!yield num)
-               stat=false
-            end
-        end
-        stat
-    end
-
-    def my_any?
-        stat=false
-        my_each do |num|
-            if(yield num)
-                stat=true
-                
-            end
-        end
-        stat
-    end
-
-    def my_none?
-        stat=true
-        my_each do |num|
-            if(yield num)
-                stat=false
-            end
-        end
-        stat
-    end
-
-    def my_count(counter=self.size)
-        p_count=0
-        if(block_given?)
-            my_each do |num|
-                if(yield num)
-                    p_count+=1
-                end
-            
-            end
-            counter=p_count
-        end
-        counter
-    end
-
-    def my_inject(acc=0)
-        
-        my_each do |num|
-            acc=yield(acc,num)
-
-        end
-        acc
-    end
-    def multiply_els
-        
-       res= my_inject(1) do |acc,num|
-            acc*num
-            
-        end
-        
-    end  
-    
-    def my_map(prop=nil)
-        new_arr=[]
-        if(prop) 
-            my_each do |num|
-                new_arr.push(prop.call(num))
-            end
-        
-        else
-            my_each do |num|
-              new_arr.push(yield num)
-            end
-        end
-
-        new_arr
-    
-        
-    end
-       
-
-
-
-        
-            
-        
-
-
+    new_arr
+  end
 end
 
+arr = [4, 6, 3]
 
-arr=[4,6,3]
-
-# p arr.my_all?{|num| num%2==0} 
+# p arr.my_all?{|num| num%2==0}
 # p arr.my_count(7)
 
-# p (1..4).map { |i| i*i } 
-# p arr.my_inject(0) { |sum, n| sum + n }  
+# p (1..4).map { |i| i*i }
+# p arr.my_inject(0) { |sum, n| sum + n }
 
 p arr.multiply_els
-prop=Proc.new{|x| x*2}
+prop = proc { |x| x * 2 }
 p arr.my_map(prop)
