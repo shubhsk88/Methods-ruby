5de8401d55477d8629aaf36004ba9e3d317d6fe7
shubhsk88 <shubham88singh@gmail.com>
2020-05-01 01:29:21 +0530

    Fixing th rubocop errors

--- method.rb
+++ method.rb
@@ -1,56 +1,51 @@
 module Enumerable
   def my_each
     i = 0
-    arr= self.to_a
-    if block_given?
-      while i <arr.size
-        yield arr[i]
-        i += 1
-      end
-    else
-      return to_enum(:my_each)
+    arr = to_a
+    return to_enum(:my_each) unless block_given?
+
+    while i < arr.size
+      yield arr[i]
+      i += 1
     end
     self
   end
 
   def my_each_with_index
     i = 0
-    arr= self.to_a
-    if block_given?
-      while i <arr.size
-        yield arr[i],i
-        i += 1
-      end
-    else
-      return to_enum(:my_each)
+    arr = to_a
+    return to_enum(:my_each_with_index) unless block_given?
+
+    while i < arr.size
+      yield arr[i], i
+      i += 1
     end
+
     self
   end
 
   def my_select
     new_arr = []
-    if block_given?
-      my_each do |num|
-        new_arr.push(num) if yield num
-      end
-    else 
-      return to_enum(:my_select)  
+    return to_enum(:my_select) unless block_given?
+
+    my_each do |num|
+      new_arr.push(num) if yield num
     end
-    
+
     new_arr
   end
 
-  def my_all?(arg=nil)
+  def my_all?(arg = nil)
     stat = true
 
-    if(arg)
-      if(arg.class==Class)
+    if arg
+      if arg.class == Class
         my_each do |num|
-          stat= num.is_a?(arg)
+          stat = num.is_a?(arg)
         end
-      else 
+      else
         my_each do |num|
-          stat= num==arg
+          stat = num == arg
         end
       end
 
@@ -59,65 +54,62 @@ module Enumerable
         stat = false unless yield num
       end
     else
-      return stat  
-    end 
+      return stat
+    end
     stat
   end
 
-  def my_any?(arg=nil)
+  def my_any?(arg = nil)
     stat = false
 
-    if(arg)
-      if(arg.class==Class)
+    if arg
+      if arg.class == Class
         my_each do |num|
-          stat= num.is_a?(arg)
+          stat = num.is_a?(arg)
         end
-      else 
+      else
         my_each do |num|
-          stat= num==arg
+          stat = num == arg
         end
       end
-        
+
     elsif block_given?
       my_each do |num|
         stat = true if yield num
       end
     else
-      return stat  
-    end 
+      my_each do |num|
+        bool = !num
+        stat = !bool
+      end
+    end
     stat
-    
   end
 
-  def my_none?(arg=nil)
+  def my_none?(arg = nil)
     stat = true
 
-    if(arg)
-      if(arg.class==Class)
+    if arg
+      if arg.class == Class
         my_each do |num|
-          stat= num.is_a?(arg)
+          stat = num.is_a?(arg)
         end
-      else 
+      else
         my_each do |num|
-          stat= !num==arg
+          stat = num != arg
         end
       end
-        
+
     elsif block_given?
       my_each do |num|
         stat = false if yield num
       end
     else
-      return stat  
-    end 
+      my_each do |num|
+        stat = !num
+      end
+    end
     stat
-    
-    
-    # stat = true
-    # my_each do |num|
-    #   stat = false if yield num
-    # end
-    # stat
   end
 
   def my_count(counter = size)
@@ -146,26 +138,23 @@ module Enumerable
 
   def my_map(prop = nil)
     new_arr = []
-    if block_given?
+    return to_enum(:my_map) unless block_given?
 
-      if prop
-        my_each do |num|
-          new_arr.push(prop.call(num))
-        end
-
-      else
-        my_each do |num|
-          new_arr.push(yield num)
-        end
+    if prop
+      my_each do |num|
+        new_arr.push(prop.call(num))
       end
+
     else
-      return to_enum(:my_map) 
-    end  
+      my_each do |num|
+        new_arr.push(yield num)
+      end
+    end
+
     new_arr
   end
 end
-
-arr = [4, 6, 3]
+# arr = [4, 6, 3]
 
 # p arr.my_all?{|num| num%2==0}
 # p arr.my_count(7)
@@ -178,5 +167,3 @@ arr = [4, 6, 3]
 # p arr.multiply_els
 # prop = proc { |x| x * 2 }
 # p arr.my_map(prop)
-
-
